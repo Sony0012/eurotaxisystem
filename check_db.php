e1fe4c90edd9548ea01c492129de47d3ca595d54
Sony0012 <sonysunico02@gmail.com>
2026-04-29 00:05:45 +0800

    Update check_db.php via PHP Script

--- check_db.php
+++ check_db.php
@@ -2,6 +2,6 @@
 require 'vendor/autoload.php';
 $app = require_once 'bootstrap/app.php';
 $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-$res = DB::select('SHOW CREATE TABLE expenses');
+$res = DB::select('SHOW CREATE TABLE salaries');
 print_r($res[0]);
 ?>
