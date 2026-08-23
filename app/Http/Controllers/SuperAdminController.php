1f864c6798c1d5aa5fc0e2145156b2f237266853
Sony <sony@eurotaxi.com>
2026-08-23 13:40:22 +0800

    fix(activity): import Schema facade in SuperAdminController to resolve class not found error

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
 use App\Models\User;
 use App\Models\LoginAudit;
 use App\Models\SystemSetting;
